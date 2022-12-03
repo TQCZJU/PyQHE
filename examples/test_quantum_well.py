Fix the subtle sign problem

examples/test_quantum_well.py
@@ -5,7 +5,7 @@ from scipy.stats import norm
 from matplotlib import pyplot as plt
 
 from pyqhe.core.structure import Layer, Structure1D
-from pyqhe.schrodinger_poisson import SchrodingerPoisson
+from pyqhe.schrodinger_poisson import SchrodingerPoisson, SchrodingerShooting
 from pyqhe.equation.poisson import PoissonFDM, PoissonODE
 from pyqhe.equation.schrodinger import SchrodingerMatrix
 

pyqhe/equation/poisson.py
@@ -34,23 +34,24 @@ class PoissonODE(PoissonSolver):
     """
 
     def __init__(self, grid: np.ndarray, charge_density: np.ndarray,
-                 eps: np.ndarray) -> None:
+                 eps: np.ndarray, *args, **kwargs) -> None:
         super().__init__()
-
+        if isinstance(grid, list):  # nd grid
+            grid = grid[0]
         self.grid = grid
         self.charge_density = charge_density
         self.eps = eps
 
     def calc_poisson(self, **kwargs):
         """Calculate electric field."""
-
+        # Gauss's law
         d_z = cumulative_trapezoid(const.q * self.charge_density,
                                    self.grid,
                                    initial=0)
         self.e_field = d_z / self.eps
         # integral the potential
         # note here we put a electron, dV/dz = E
-        self.v_potential = cumulative_trapezoid(self.e_field,
+        self.v_potential = cumulative_trapezoid(-1.0 * self.e_field,
                                                 self.grid,
                                                 initial=0)
 
@@ -135,7 +136,7 @@ class PoissonFDM(PoissonSolver):
             a_mat_list.append(
                 d_opt.reshape(np.prod(self.dim), np.prod(self.dim)))
         a_mat = np.sum(a_mat_list, axis=0)
-        b_vec = const.q * self.charge_density.flatten()
+        b_vec = -1.0 * const.q * self.charge_density.flatten()
 
         if self.bound_dirichlet is not None:
             delta = self.grid[0][1] - self.grid[0][0]
@@ -151,7 +152,7 @@ class PoissonFDM(PoissonSolver):
 
         self.v_potential = solve(a_mat, b_vec).reshape(self.dim)
         # calculate gradient of potential
-        self.e_field = np.gradient(self.v_potential)
+        self.e_field = np.gradient(-1.0 * self.v_potential)
         return self.v_potential
 
 

pyqhe/equation/schrodinger.py
@@ -32,8 +32,9 @@ class SchrodingerSolver(ABC):
 class SchrodingerShooting(SchrodingerSolver):
     """Shooting method solver for calculation Schrodinger equation."""
 
-    def __init__(self, grid: np.ndarray, v_potential, cb_meff) -> None:
-
+    def __init__(self, grid: np.ndarray, v_potential, cb_meff, *args, **kwargs) -> None:
+        if isinstance(grid, list):  # nd grid
+            grid = grid[0]
         # Schrodinger equation's parameters
         self.v_potential = v_potential
         self.cb_meff = cb_meff

pyqhe/schrodinger_poisson.py
@@ -130,8 +130,10 @@ class SchrodingerPoisson:
             accumu_elec = np.trapz(accumu_elec, x=grid)
         norm = self.accumulate_q / accumu_elec
         elec_density *= norm
-        # Let dopants density minus electron density
-        net_density = self.doping - elec_density
+        # noted we consider a electron in the conductive band, and high electron
+        # density are repulsive to an electron.
+        net_density = elec_density - self.doping
+        # the sign will be conflicted with general potential definition due to self.fi
 
         return net_density
 