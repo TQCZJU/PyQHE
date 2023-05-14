2d2d3f73c6a227b21da1dff6b41ce427f7dc7cb1
allegro0132 <34514398+allegro0132@users.noreply.github.com>
2023-05-14 21:50:32 +0800

    Add Fiori's n-d wave function

--- pyqhe/equation/schrodinger.py
+++ pyqhe/equation/schrodinger.py
@@ -33,7 +33,8 @@ class SchrodingerSolver(ABC):
 class SchrodingerShooting(SchrodingerSolver):
     """Shooting method solver for calculation Schrodinger equation."""
 
-    def __init__(self, grid: np.ndarray, v_potential, cb_meff, *args, **kwargs) -> None:
+    def __init__(self, grid: np.ndarray, v_potential, cb_meff, *args,
+                 **kwargs) -> None:
         if isinstance(grid, list):  # nd grid
             grid = grid[0]
         # Schrodinger equation's parameters
@@ -170,13 +171,18 @@ class SchrodingerMatrix(SchrodingerSolver):
         else:
             raise ValueError('The dimension of cb_meff is not match.')
         if quantum_region is None:
-            self.quantum_region = [np.ones_like(grid) == 1 for grid in self.grid]
+            self.quantum_region = [
+                np.ones_like(grid) == 1 for grid in self.grid
+            ]
         else:
             if not isinstance(quantum_region, list):  # 1d grid
                 quantum_region = [quantum_region]
             self.quantum_region = quantum_region
-        self.quantum_dim = [len(g[m]) for g, m in zip(self.grid, self.quantum_region)]
-        self.quantum_musk = reduce(np.outer, self.quantum_region).reshape(self.quantum_dim)
+        self.quantum_dim = [
+            len(g[m]) for g, m in zip(self.grid, self.quantum_region)
+        ]
+        self.quantum_musk = reduce(np.outer,
+                                   self.quantum_region).reshape(self.dim)
         if bound_period is None:
             self.bound_period = [None] * len(self.dim)
         elif len(bound_period) == len(self.dim):
@@ -211,12 +217,14 @@ class SchrodingerMatrix(SchrodingerSolver):
         k_mat_list = []
         for loc, _ in enumerate(self.dim):
             mat = self.build_kinetic_operator(loc)
-            kron_list = [np.eye(idim) for idim in self.quantum_dim[:loc]] + [mat] + [
-                np.eye(idim) for idim in self.quantum_dim[loc + 1:]
-            ] + [1]  # auxiliary element for 1d solver
+            kron_list = [np.eye(idim) for idim in self.quantum_dim[:loc]] + [
+                mat
+            ] + [np.eye(idim) for idim in self.quantum_dim[loc + 1:]
+                ] + [1]  # auxiliary element for 1d solver
             delta = self.grid[loc][1] - self.grid[loc][0]
             # coeff = -0.5 * const.hbar**2 * self.cb_meff * self.beta**2 / delta**2
-            coeff = -0.5 * const.hbar**2 / self.cb_meff[self.quantum_musk].reshape(self.quantum_dim) / delta**2
+            coeff = -0.5 * const.hbar**2 / self.cb_meff[
+                self.quantum_musk].reshape(self.quantum_dim) / delta**2
             # construct n-d kinetic operator by tensor product
             k_opt = tensor(*kron_list)
             # tensor contraction
@@ -224,7 +232,9 @@ class SchrodingerMatrix(SchrodingerSolver):
                               np.arange(len(self.quantum_dim * 2)), coeff,
                               np.arange(len(self.quantum_dim)),
                               np.arange(len(self.quantum_dim * 2)))
-            k_mat_list.append(k_opt.reshape(np.prod(self.quantum_dim), np.prod(self.quantum_dim)))
+            k_mat_list.append(
+                k_opt.reshape(np.prod(self.quantum_dim),
+                              np.prod(self.quantum_dim)))
             # k_mat_list.append(k_opt / coeff)
         k_mat = np.sum(k_mat_list, axis=0)
         v_mat = self.build_potential_operator()
@@ -244,7 +254,8 @@ class SchrodingerMatrix(SchrodingerSolver):
         #                     np.arange(len(self.dim * 2)), coeff,
         #                     np.arange(len(self.dim)),
         #                     np.arange(len(self.dim * 2)))
-        eig_vec = eig_vec.reshape(np.prod(self.quantum_dim), np.prod(self.quantum_dim))
+        eig_vec = eig_vec.reshape(np.prod(self.quantum_dim),
+                                  np.prod(self.quantum_dim))
         # eig_vec = np.einsum('ij,i->ij', eig_vec, 1 / self.cb_meff / self.beta)
         wave_func = []
         for musk_vec in eig_vec.T:
@@ -261,12 +272,168 @@ class SchrodingerMatrix(SchrodingerSolver):
         return eig_val, np.array(wave_func)
 
 
+class SchrodingerFiori(SchrodingerSolver):
+    """Approach to multi-body 3d wave function of electrons system.
+    The method from Journal of Computational Electronics 1: 39–42, 2002. The 3D
+    schrodinger equation can be separated into 1d and 2d part.
+    """
+
+    def __init__(self,
+                 grid: Union[List[np.ndarray], np.ndarray],
+                 v_potential: np.ndarray,
+                 cb_meff: np.ndarray,
+                 bound_period: list = None,
+                 quantum_region=None) -> None:
+        super().__init__()
+
+        if not isinstance(grid, list):  # 1d grid
+            grid = [grid]
+        self.grid = grid
+        self.dim = [grid_axis.shape[0] for grid_axis in self.grid]
+        # check matrix dim
+        if v_potential.shape == tuple(self.dim) or len(self.dim) == 1:
+            self.v_potential = v_potential
+        else:
+            raise ValueError('The dimension of v_potential is not match')
+        if cb_meff.shape == tuple(self.dim) or len(self.dim) == 1:
+            self.cb_meff = cb_meff
+        else:
+            raise ValueError('The dimension of cb_meff is not match.')
+        if quantum_region is None:
+            self.quantum_region = [
+                np.ones_like(grid) == 1 for grid in self.grid
+            ]
+        else:
+            if not isinstance(quantum_region, list):  # 1d grid
+                quantum_region = [quantum_region]
+            self.quantum_region = quantum_region
+        self.quantum_dim = [
+            len(g[m]) for g, m in zip(self.grid, self.quantum_region)
+        ]
+        self.quantum_musk = reduce(np.outer, self.quantum_region).reshape(
+            self.quantum_dim)
+        if bound_period is None:
+            self.bound_period = [None] * len(self.dim)
+        elif len(bound_period) == len(self.dim):
+            self.bound_period = bound_period
+        else:
+            raise ValueError('The dimension of bound_period is not match.')
+
+    def build_kinetic_operator(self):
+        """Build 1D time independent Schrodinger equation kinetic operator.
+
+        Args:
+            dim: dimension of kinetic operator.
+        """
+        # construct the kinetic operator for z axis equation
+        mat_d = -2 * np.eye(self.dim[-1]) + np.eye(self.dim[-1], k=-1) + np.eye(
+            self.dim[-1], k=1)
+        if self.bound_period[-1]:  # add period boundary condition
+            mat_d[0, -1] = 1
+            mat_d[-1, 0] = 1
+        return mat_d
+
+    def build_potential_operator(self, lateral_idx):
+        """Build 1D time independent Schrodinger equation potential operator.
+
+        Args:
+            lateral_idx: index along the lateral direction
+        """
+
+        return np.diag(self.v_potential[lateral_idx].flatten())
+
+    def hamiltonian(self, lateral_idx):
+        """Construct time independent Schrodinger equation."""
+        # construct V and cb_meff matrix
+        # discrete laplacian
+
+        k_opt = self.build_kinetic_operator()
+        delta = self.grid[-1][1] - self.grid[-1][0]
+        coeff = -0.5 * const.hbar**2 / self.cb_meff[lateral_idx] / delta**2
+        # row-wise multiplication
+        k_opt = k_opt * coeff.reshape(-1, 1)
+        v_mat = self.build_potential_operator(lateral_idx)
+
+        return k_opt + v_mat
+
+    def calc_evals(self):
+        # we use a set of index describe the point in lateral direction
+        num_lateral = np.prod(self.dim[:-1])
+        # initialize data set
+        eigval_set = []
+        for flat_idx in range(num_lateral):
+            multi_idx = np.unravel_index(flat_idx, self.dim[:-1])
+            # collect related data for Schrodinger equation
+            ham = self.hamiltonian(multi_idx)
+            # calculate eigensystems
+            eig_val = sciLA.eigh(ham, eigvals_only=True)
+            eigval_set.append(eig_val)
+        # reshape the set of eigensystems
+        # the multi-dimension index of eigval_set is [lateral_idx0, lateral_idx1, i-th eigenval]
+        eigval_set = np.array(eigval_set).reshape(self.dim)
+
+        return eigval_set
+
+    def calc_esys(self):
+        # we use a set of index describe the point in lateral direction
+        num_lateral = np.prod(self.dim[:-1])
+        # initialize data set
+        eigval_set = []
+        wave_func_set = []
+        for flat_idx in range(num_lateral):
+            multi_idx = np.unravel_index(flat_idx, self.dim[:-1])
+            # collect related data for Schrodinger equation
+            ham = self.hamiltonian(multi_idx)
+            # calculate eigensystems
+            eig_val, eig_vec = sciLA.eigh(ham)
+            eigval_set.append(eig_val)
+            # assemble eigen-energy set
+            wave_func = []
+            for vec in eig_vec.T:
+                # normalize
+                norm = vec * np.conj(vec)
+                norm = np.trapz(norm, self.grid[-1])
+                wave_func.append(vec / np.sqrt(norm))
+            wave_func = np.array(wave_func)
+            wave_func_set.append(wave_func)
+        # reshape the set of eigensystems
+        # the multi-dimension index of eigval_set is [lateral_idx0, lateral_idx1, i-th eigenval]
+        eigval_set = np.array(eigval_set).reshape(self.dim)
+        # For wave_func_set, we use [lateral_idx0, lateral_idx1, i-th eigenvec, growth axis]
+        wave_func_set = np.array(wave_func_set).reshape(self.dim + [
+            self.dim[-1],
+        ])
+
+        return eigval_set, wave_func_set
+
+
 # %%
 # QuickTest
 if __name__ == '__main__':
     from matplotlib import pyplot as plt
     from matplotlib import cm
 
+    x = np.linspace(-1, 1, 20)
+    y = np.linspace(-1, 1, 30)
+    z = np.linspace(-1, 1, 100)
+    xv, yv, zv = np.meshgrid(x, y, z, indexing='ij')
+    z_barrier = (zv <= -0.5) + (zv >= 0.5)
+    v_potential = np.zeros([20, 30, 100])
+    v_potential[z_barrier] = 10  # set barrier
+    sol = SchrodingerFiori([x, y, z], v_potential,
+                           np.ones_like(v_potential) * const.m_e)
+    eig_val, wf = sol.calc_esys()
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    surf = ax.plot_surface(yv[0],
+                           zv[0],
+                           wf[0, :, 2, :],
+                           cmap=cm.coolwarm,
+                           linewidth=0,
+                           antialiased=False)
+    ax.set_xlabel('x')
+    ax.set_ylabel('z')
+    ax.set_zlabel(r'$\psi(r,z)$')
+    # %%
     x = np.linspace(-1, 1, 50)
     y = np.linspace(-1, 1, 55)
     xv, yv = np.meshgrid(x, y, indexing='ij')
@@ -274,7 +441,8 @@ if __name__ == '__main__':
     y_barrier = (yv <= -0.5) + (yv >= 0.5)
     v_potential = np.zeros([50, 55])
     v_potential[x_barrier + y_barrier] = 1  # set barrier
-    sol = SchrodingerMatrix([x, y], v_potential, np.ones_like(v_potential) * const.m_e)
+    sol = SchrodingerMatrix([x, y], v_potential,
+                            np.ones_like(v_potential) * const.m_e)
     eig_val, wf = sol.calc_esys()
     # %%
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
@@ -284,19 +452,20 @@ if __name__ == '__main__':
                            cmap=cm.coolwarm,
                            linewidth=0,
                            antialiased=False)
-
-    plt.show()
     # %%
-    grid = np.linspace(0, 10, 100)
+    grid = np.linspace(-1, 1, 100)
     psi = np.zeros(grid.shape)
-    v_potential = np.ones(grid.shape)
+    v_potential = np.zeros(grid.shape)
     # Quantum well
-    v_potential[40:61] = 0
-    quantum_region = (grid < 5) * (grid > 4)
+    z_barrier = (grid <= -0.5) + (grid >= 0.5)
+    v_potential[z_barrier] = 10
+    quantum_region = (grid <= 0) * (grid >0 -0.5)
     cb_meff = np.ones(grid.shape) * const.m_e
-    solver = SchrodingerMatrix(grid, v_potential, cb_meff, quantum_region=quantum_region)
+    solver = SchrodingerMatrix(grid,
+                               v_potential,
+                               cb_meff,
+                               quantum_region=quantum_region)
     val, vec = solver.calc_esys()
     plt.plot(solver.grid[0], solver.v_potential)
     plt.plot(solver.grid[0], vec[:3].T)
-    plt.show()
 # %%
